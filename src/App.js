8032cfdd85a82dd0971ea74a0ccf3d396636c83d
Vivek Yadav <yadav.2003.iamvivek@gmail.com>
2024-02-04 15:59:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React,{useState} from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
+import LoadingBar from 'react-top-loading-bar'
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+ 
+
+
 function App() {
+ const pagesize =12;
+  const [progress, setprogress] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+     <div>
+        <Router>
+        <Navbar/>
+     
+        <Routes>
+          <Route path="/" element={<News setprogress={setprogress}    pagesize={pagesize} key= "general" category = "general" country = "in" />} />
+          <Route path="/business" element={<News setprogress={setprogress}    pagesize={pagesize} key= "business" category = "business" country = "in" />} />
+          <Route path="/entertainment" element={<News setprogress={setprogress}    pagesize={pagesize} key= "entertainment" category = "entertainment" country = "in" />} />
+          <Route path="/health" element={<News setprogress={setprogress}    pagesize={pagesize} key= "health" category = "health" country = "in" />} />
+          <Route path="/science" element={<News setprogress={setprogress}    pagesize={pagesize} key= "science" category = "science" country = "in" />} />
+          <Route path="/sports" element={<News setprogress={setprogress}    pagesize={pagesize} key= "sports" category = "sports" country = "in" />} />
+          <Route path="/technology" element={<News setprogress={setprogress}    pagesize={pagesize} key= "technology" category = "technology" country = "in" />} />          
+        </Routes>
+        <LoadingBar
+        height={5}
+        color='#2597DE'
+        progress={progress}
+       
+      />
+     
+    </Router>
+     
+     </div>
+  )
 }
 
-export default App;
+export default App
+
+
